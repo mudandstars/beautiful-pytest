f53c94b9f61f1f2ef4c74c423e1d32af18b3f129
Paul Sochiera <paul@sochiera.com>
2023-12-28 14:58:59 +0100

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use std::process::{Command, Stdio};
+use std::process::{Command, Stdio, Child};
 use ansi_term::Color::{Green, Red, Black};
 use ansi_term::Style;
 use std::io::{BufRead, BufReader, Error};
+use std::env;
 
 mod pytest;
 
@@ -11,64 +12,60 @@ fn main() {
 }
 
 fn execute_pytest() {
-    let mut pytest_command = Command::new("pytest");
-    pytest_command.arg(".");
-    pytest_command.arg("-v");
-    pytest_command.stdout(Stdio::piped());
-
-    let mut child = match pytest_command.spawn() {
-        Ok(child) => child,
-        Err(e) => {
-            eprintln!("Failed to start pytest: {}", e);
-            return;
-        }
-    };
+    let mut child = run_pytest();
 
     let stdout = child.stdout.take().expect("Failed to capture stdout");
-
     let reader = BufReader::new(stdout);
     let mut current_file = "".to_string();
-    for line in reader.lines() {
-        handle_line(line, &mut current_file);
-    }
 
-    println!("");
-    match pytest_command.status() {
-            Ok(status) => println!("Command executed with status: {}", status),
-            Err(e) => eprintln!("process failed to execute: {:?}", e),
+    for (index, line) in reader.lines().enumerate() {
+        match line {
+            Ok(line) => {
+                handle_line(line, &mut current_file);
+            },
+            Err(e) => eprintln!("Error reading line: {}", e),
+        }
     }
 }
 
-fn handle_line(line: Result<String, Error>, current_file: &mut String) {
-    match line {
-        Ok(line) => {
-            // Process the line here (print or do something else)
-            if line.to_lowercase().contains("python") && line.to_lowercase().contains("pytest") {
-                println!("{}", Green.dimmed().paint(&line));
-            }
+fn handle_line(line: String, current_file: &mut String) {
+    if line.to_lowercase().contains("python") && line.to_lowercase().contains("pytest") {
+        println!("{}", Green.dimmed().paint(&line));
+    }
 
-            if pytest::line_contains_test(&line) {
-                if *current_file != pytest::extract_file_name(&line) {
-                        *current_file = pytest::extract_file_name(&line);
+    if pytest::line_contains_test(&line) {
+        if *current_file != pytest::extract_file_name(&line) {
+                *current_file = pytest::extract_file_name(&line);
 
-                        println!("");
-                        println!("{} {}.py", Style::new().on(Green).fg(Black).bold().paint(" TESTING "), *current_file);
-                }
-
-                let test_name = pytest::extract_test_name(&line).replace("test_", "").replace("_", " ");
-                if pytest::test_passed(&line) {
-                    let check_mark = '\u{2714}';
-                    println!(" {} {}", Green.paint(check_mark.to_string()), test_name);
-                } else {
-                    let x_mark = '\u{2718}';
-                    println!(" {} {}", Red.paint(x_mark.to_string()), test_name);
-                }
-            }
-
-            // println!("Pytest output: {}", line);
+                println!("");
+                println!("{} {}.py", Style::new().on(Green).fg(Black).bold().paint(" TESTING "), *current_file);
         }
-        Err(e) => {
-            eprintln!("Error reading line: {}", e);
+
+        let test_name = pytest::extract_test_name(&line).replace("test_", "").replace("_", " ");
+        if pytest::test_passed(&line) {
+            let check_mark = '\u{2714}';
+            println!(" {} {}", Green.paint(check_mark.to_string()), test_name);
+        } else {
+            let x_mark = '\u{2718}';
+            println!(" {} {}", Red.paint(x_mark.to_string()), test_name);
         }
     }
+
+    println!("Pytest output: {}", line);
+}
+
+fn run_pytest() -> Child {
+    let mut pytest_command = Command::new("pytest");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        pytest_command.arg(".");
+    } else {
+        let filter = &args[1];
+        pytest_command.arg(format!("-k {}", filter));
+    }
+    pytest_command.arg("-v");
+    // dbg!(args);
+    pytest_command.stdout(Stdio::piped());
+
+    return pytest_command.spawn().expect("Failed to start pytest");
 }
