cd78ca8e611192e6d18457927135ae1167ab8cf8
Paul Sochiera <paul@sochiera.com>
2023-12-30 15:28:05 +0100

    feat: style output

--- src/main.rs
+++ src/main.rs
@@ -8,25 +8,37 @@ mod pytest;
 
 
 fn main() {
-    execute_pytest();
-}
-
-fn execute_pytest() {
     let mut child = run_pytest();
 
     let stdout = child.stdout.take().expect("Failed to capture stdout");
     let reader = BufReader::new(stdout);
-    let mut current_file = "".to_string();
+    let mut files: Vec<pytest::File> = vec![];
+
+    for line in reader.lines() {
+        match line {
+            Ok(line) => {
+                handle_line(line, &mut files);
+            },
+            Err(e) => eprintln!("Error reading line: {}", e),
+        }
+    }
 
     let mut passed_tests_count: u32 = 0;
     let mut failed_tests_count: u32 = 0;
 
-    for (index, line) in reader.lines().enumerate() {
-        match line {
-            Ok(line) => {
-                handle_line(line, &mut current_file, &mut passed_tests_count, &mut failed_tests_count);
-            },
-            Err(e) => eprintln!("Error reading line: {}", e),
+    files.last_mut().unwrap().tests.last_mut().unwrap().error_type = Some(String::from("NameError"));
+    files.last_mut().unwrap().tests.last_mut().unwrap().short_error_description = Some(String::from("NameError: name 'asdf' is not defined"));
+    // files.last_mut().unwrap().tests.last_mut().unwrap().short_error_description = Some(String::from("FAILED test_test2.py::test_passed_test - NameError: name 'asdf' is not defined"));
+    files.last_mut().unwrap().tests.last_mut().unwrap().full_error = Some(String::from(" def test_passed_test():\n\t>       assert 1 == asdf\n\tE       NameError: name 'asdf' is not defined\n\n\ttest_test2.py:6: NameError"));
+
+    for file in files.iter() {
+        for test in file.tests.iter() {
+            if test.error_type.is_none() {
+                passed_tests_count +=1;
+            } else {
+                test.print_error(String::from(file.get_name()));
+                failed_tests_count +=1;
+            }
         }
     }
 
@@ -38,32 +50,36 @@ fn execute_pytest() {
     );
 }
 
-fn handle_line(line: String, current_file: &mut String, passed_tests_count: &mut u32, failed_tests_count: &mut u32) {
+
+fn handle_line(line: String, files: &mut Vec<pytest::File>) {
     if line.to_lowercase().contains("python") && line.to_lowercase().contains("pytest") {
         println!("{}", Green.dimmed().paint(&line));
     }
 
     if pytest::line_contains_test(&line) {
-        if *current_file != pytest::extract_file_name(&line) {
-                *current_file = pytest::extract_file_name(&line);
+        // if let Some(file) = files.last() {
+        if files.last().is_none() || files.last().unwrap().get_name() != pytest::extract_file_name(&line) {
+            files.push(pytest::File::new(pytest::extract_file_name(&line)));
 
-                println!("");
-                println!("{} {}.py", Style::new().on(Green).fg(Black).bold().paint(" TESTING "), *current_file);
+            println!("");
+            println!("{} {}", Style::new().on(Green).fg(Black).bold().paint(" TESTING "), files.last().unwrap().get_name());
         }
 
         let test_name = pytest::extract_test_name(&line).replace("test_", "").replace("_", " ");
+        files.last_mut().unwrap().tests.push(pytest::Test::new(test_name.clone()));
+
         if pytest::test_passed(&line) {
             let check_mark = '\u{2714}';
-            *passed_tests_count += 1;
             println!(" {} {}", Green.paint(check_mark.to_string()), test_name);
         } else {
             let x_mark = '\u{2718}';
-            *failed_tests_count += 1;
+            files.last_mut().unwrap().tests.last_mut().unwrap().error_type = Some(String::from("something"));
+
             println!(" {} {}", Red.paint(x_mark.to_string()), test_name);
         }
     }
 
-    println!("Pytest output: {}", line);
+    // println!("Pytest output: {}", line);
 }
 
 fn run_pytest() -> Child {
--- src/pytest.rs
+++ src/pytest.rs
@@ -1,3 +1,56 @@
+use ansi_term::Color::{Red, Black};
+use ansi_term::Style;
+
+const FAILED_TEST_HEAD_LINE_SEPARATOR: &str = "________________________________";
+const FAILED_TEST_SECTION_LINE_SEPARATOR: &str = "===================================";
+
+pub struct File {
+    name: String,
+    pub tests: Vec<Test>
+}
+
+impl File {
+    pub fn new(name: String) -> File {
+        return File {
+            name,
+            tests: vec![],
+        }
+    }
+
+    pub fn get_name(&self) -> &str {
+        &self.name
+    }
+}
+
+pub struct Test {
+    name: String,
+    pub error_type: Option<String>,
+    pub short_error_description: Option<String>,
+    pub full_error: Option<String>,
+}
+
+impl Test {
+    pub fn new(name: String) -> Test {
+        Test {
+            name,
+            error_type: None,
+            short_error_description: None,
+            full_error: None,
+        }
+    }
+
+    pub fn print_error(&self, file_name: String) {
+        if self.error_type.is_some() && self.short_error_description.is_some() && self.full_error.is_some() {
+            println!("");
+            println!("{}\t{}::{}\t\t\t\t{}", Style::new().on(Red).fg(Black).bold().paint(" FAILED "), file_name, self.name, Style::new().on(Red).fg(Black).bold().paint(String::from(" ") + self.error_type.as_ref().unwrap() + " "));
+            println!(" {}", Red.paint(self.short_error_description.as_ref().unwrap()));
+            println!("");
+            println!(" {}", self.full_error.as_ref().unwrap());
+            println!("");
+        }
+    }
+}
+
 pub fn line_contains_test(line: &str) -> bool {
     return line.contains(".py::") && (line.contains(" PASSED") || line.contains(" FAILED"));
 }
@@ -23,5 +76,5 @@ pub fn extract_test_name(line: &str) -> String {
 pub fn extract_file_name(line: &str) -> String {
     let end = line.rfind(".py").unwrap();
 
-    return line[0..end].to_string();
+    return line[0..end].to_string() + ".py";
 }
